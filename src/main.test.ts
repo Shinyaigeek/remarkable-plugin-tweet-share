a476b421e011cbcccd00133cded1c3a9431c9c47
shinyaigeek <PhilisPaxil@gmail.com>
2021-02-21 21:10:38 +0900

    Chore: add test for plugin

---
+++ src/main.test.ts
@@ -0,0 +1,20 @@
+import { Remarkable } from "remarkable";
+import { tweetMacroPlugin } from "./main";
+
+describe("remarkdown-plugin-tweet-share", () => {
+  test("parse !tweet macro", () => {
+    const src = `## h2
+### h3
+
+!tweet[](https://twitter.com/Shinyaigeek/status/1363426871950536705?s=20)
+`;
+
+    const md = new Remarkable();
+    md.use(tweetMacroPlugin)
+    console.log(md.render(src))
+    expect(md.render(src)).toEqual(`<h2>h2</h2>
+<h3>h3</h3>
+<p><a href="https://twitter.com/Shinyaigeek/status/1363426871950536705?s=20"><img src="https://tweet2image.vercel.app/1363426871950536705.jpg" alt=""></a></p>
+`);
+  });
+});
