0b9b17fbe7308105c798386f48bffcef5f7c8f52
shinyaigeek <PhilisPaxil@gmail.com>
2021-02-22 19:04:10 +0900

    Chore: add test for main

--- src/main.test.ts
+++ src/main.test.ts
@@ -17,4 +17,39 @@ describe("remarkdown-plugin-tweet-share", () => {
 <p><a href="https://twitter.com/Shinyaigeek/status/1363426871950536705?s=20"><img src="https://tweet2image.vercel.app/1363426871950536705.jpg" alt=""></a></p>
 `);
   });
+
+  test("parse !tweet macro", () => {
+    const src = `## h2
+### h3
+
+!tweet[](https://twitter.com/Shinyaigeek/status/1363426871950536705?s=20)
+
+![](asdf)
+`;
+
+    const md = new Remarkable();
+    md.use(tweetMacroPlugin)
+    console.log(md.render(src))
+    expect(md.render(src)).toEqual(`<h2>h2</h2>
+<h3>h3</h3>
+<p><a href="https://twitter.com/Shinyaigeek/status/1363426871950536705?s=20"><img src="https://tweet2image.vercel.app/1363426871950536705.jpg" alt=""></a></p>
+<p><img src="asdf" alt=""></p>
+`);
+  });
+
+  test("cancel parsing !tweet", () => {
+    const src = `## h2
+### h3
+
+!tweet[
+`;
+
+    const md = new Remarkable();
+    md.use(tweetMacroPlugin)
+    console.log(md.render(src))
+    expect(md.render(src)).toEqual(`<h2>h2</h2>
+<h3>h3</h3>
+<p>!tweet[</p>
+`);
+  });
 });
